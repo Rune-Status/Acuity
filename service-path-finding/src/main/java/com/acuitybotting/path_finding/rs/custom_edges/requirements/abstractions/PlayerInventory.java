4dd648d135572ff03931fdb4ab2e0d58cb44915f
Zachary Herridge <zgherridge@gmail.com>
2018-07-25 17:11:58 -0500

    Pathing fixes.

--- service-path-finding/src/main/java/com/acuitybotting/path_finding/rs/custom_edges/requirements/abstractions/PlayerInventory.java
+++
@@ -1,7 +0,0 @@
-package com.acuitybotting.path_finding.rs.custom_edges.requirements.abstractions;
-
-/**
- * Created by Zachary Herridge on 6/21/2018.
- */
-public interface PlayerInventory {
-}
--- service-path-finding/src/main/java/com/acuitybotting/path_finding/rs/custom_edges/requirements/abstractions/PlayerLevels.java
+++ service-path-finding/src/main/java/com/acuitybotting/path_finding/rs/custom_edges/requirements/abstractions/PlayerLevels.java
@@ -4,4 +4,10 @@ package com.acuitybotting.path_finding.rs.custom_edges.requirements.abstractions
  * Created by Zachary Herridge on 6/21/2018.
  */
 public interface PlayerLevels {
+
+    int getLevel(String skillName);
+
+    default boolean hasLevel(String skillName, int lowerBound){
+        return getLevel(skillName) >= lowerBound;
+    }
 }
