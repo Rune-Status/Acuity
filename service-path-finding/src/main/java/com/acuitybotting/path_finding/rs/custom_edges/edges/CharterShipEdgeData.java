5dd7f3c622f4ed3be1bf0d560548f788f1de3e05
Jasper Ketelaar <Jasperketelaar@kpnmail.nl>
2018-07-26 12:21:08 +0200

    Added charter ship edge data

---
+++ service-path-finding/src/main/java/com/acuitybotting/path_finding/rs/custom_edges/edges/CharterShipEdgeData.java
@@ -0,0 +1,10 @@
+package com.acuitybotting.path_finding.rs.custom_edges.edges;
+
+import com.acuitybotting.path_finding.rs.custom_edges.CustomEdgeData;
+
+public class CharterShipEdgeData extends CustomEdgeData {
+
+    public CharterShipEdgeData() {
+
+    }
+}
