ea3cea018b8f5e22aabadbac9592b5463c836953
Zach Herridge <zgherridge@gmail.com>
2018-07-30 12:15:01 -0500

    Started reverse AStar.

--- service-path-finding/src/main/java/com/acuitybotting/path_finding/algorithms/astar/implmentation/ReverseAStarImplementation.java
+++ service-path-finding/src/main/java/com/acuitybotting/path_finding/algorithms/astar/implmentation/ReverseAStarImplementation.java
@@ -10,7 +10,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
-@SuppressWarnings("Duplicates")
 @Slf4j
 @Getter
 public class ReverseAStarImplementation extends AStarImplementation{
