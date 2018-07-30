f74abac977f21d87c1d2123fbde357038e693617
Zach Herridge <zgherridge@gmail.com>
2018-07-30 14:00:41 -0500

    Better data.

--- service-path-finding/src/main/java/com/acuitybotting/path_finding/service/domain/abstractions/player/RsPlayer.java
+++ service-path-finding/src/main/java/com/acuitybotting/path_finding/service/domain/abstractions/player/RsPlayer.java
@@ -1,5 +1,6 @@
 package com.acuitybotting.path_finding.service.domain.abstractions.player;
 
+import com.acuitybotting.path_finding.rs.domain.location.Location;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -14,9 +15,17 @@ import java.util.Map;
 @ToString
 public class RsPlayer {
 
+    private String ign;
+    private String email;
+
     private int combatLevel;
     private int spellBook;
+    private int world;
+    private Location location;
+
     private Map<String, Integer> levels;
     private Map<Integer, Integer> inventory;
     private Map<Integer, Integer> equipment;
+    private Map<Integer, Integer> bank;
+    private Map<Integer, Integer> questProgress;
 }
