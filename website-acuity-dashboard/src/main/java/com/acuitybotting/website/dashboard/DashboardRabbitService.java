0ee37fafa96f06f7e2b4e26de3ea295296f442c8
Zachary Herridge <zgherridge@gmail.com>
2018-08-03 18:02:25 -0500

    Custom edge fixes.

--- website-acuity-dashboard/src/main/java/com/acuitybotting/website/dashboard/DashboardRabbitService.java
+++ website-acuity-dashboard/src/main/java/com/acuitybotting/website/dashboard/DashboardRabbitService.java
@@ -1,6 +1,5 @@
 package com.acuitybotting.website.dashboard;
 
-import com.acuitybotting.data.flow.messaging.services.client.implementation.rabbit.RabbitChannel;
 import com.acuitybotting.data.flow.messaging.services.client.implementation.rabbit.RabbitClient;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,8 +30,6 @@ public class DashboardRabbitService implements CommandLineRunner {
     private final ApplicationEventPublisher publisher;
     private final ConfigurableApplicationContext applicationContext;
 
-    private RabbitChannel rabbitChannel;
-
     @Autowired
     public DashboardRabbitService(ApplicationEventPublisher publisher, ConfigurableApplicationContext applicationContext) {
         this.publisher = publisher;
@@ -43,31 +40,7 @@ public class DashboardRabbitService implements CommandLineRunner {
         try {
             RabbitClient rabbitClient = new RabbitClient();
             rabbitClient.auth(host, username, password);
-
-/*            rabbitClient.getListeners().add(new ClientListenerAdapter(){
-                @Override
-                public void onConnect(MessagingClient client) {
-       *//*             rabbitChannel = (RabbitChannel) client.openChannel();
-                    rabbitChannel.getListeners().add(new ChannelListenerAdapter(){
-                        @Override
-                        public void onConnect(MessagingChannel channel) {
-                            try {
-                                channel.createQueue("testQueue")
-                                        .withListener(messageEvent -> RootLayout.getGlobalEventBus().post(messageEvent.getMessage()))
-                                        .create()
-                                        .bind("amq.rabbitmq.event", "queue.#")
-                                        .bind("acuitybotting.general", "user.*.services.bot-control.set-status")
-                                        .consume(true);
-                            } catch (MessagingException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    });
-                    rabbitChannel.open();*//*
-                }
-            });*/
-
-            rabbitClient.connect("ADB_" + UUID.randomUUID().toString());
+            rabbitClient.connect("ADB_001_" + UUID.randomUUID().toString());
         }
         catch (Throwable e){
             log.error("Error during dashboard RabbitMQ setup.", e);
