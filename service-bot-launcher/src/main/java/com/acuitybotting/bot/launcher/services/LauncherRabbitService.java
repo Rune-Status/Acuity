c99fcfd6868e3a433c9286ef06e2c621ab5c799f
Zach Herridge <zgherridge@gmail.com>
2018-08-06 07:23:16 -0500

    Launcher init

---
+++ service-bot-launcher/src/main/java/com/acuitybotting/bot/launcher/services/LauncherRabbitService.java
@@ -0,0 +1,16 @@
+package com.acuitybotting.bot.launcher.services;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by Zachary Herridge on 8/6/2018.
+ */
+@Service
+public class LauncherRabbitService implements CommandLineRunner {
+
+    @Override
+    public void run(String... strings) throws Exception {
+
+    }
+}
