074aab5bab52fa21842e7b9b0f4b1598aec09de0
Zach Herridge <zgherridge@gmail.com>
2018-08-06 13:38:23 -0500

    Pathfinding docker update.

--- service-discord-bot/src/main/java/com/acuitybotting/discord/bot/services/rabbit/DiscordBotRabbitService.java
+++ service-discord-bot/src/main/java/com/acuitybotting/discord/bot/services/rabbit/DiscordBotRabbitService.java
@@ -1,10 +1,17 @@
 package com.acuitybotting.discord.bot.services.rabbit;
 
+import com.acuitybotting.data.flow.messaging.services.client.MessagingChannel;
+import com.acuitybotting.data.flow.messaging.services.client.implementation.rabbit.RabbitClient;
 import com.acuitybotting.data.flow.messaging.services.client.implementation.rabbit.management.RabbitManagement;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 /**
  * Created by Zachary Herridge on 8/6/2018.
@@ -12,7 +19,7 @@ import org.springframework.stereotype.Service;
 @Service
 @Getter
 @Slf4j
-public class DiscordBotRabbitService {
+public class DiscordBotRabbitService implements CommandLineRunner {
 
     @Value("${rabbit.host}")
     private String host;
@@ -21,11 +28,38 @@ public class DiscordBotRabbitService {
     @Value("${rabbit.password}")
     private String password;
 
+    private final ApplicationEventPublisher publisher;
+
+    @Autowired
+    public DiscordBotRabbitService(ApplicationEventPublisher publisher) {
+        this.publisher = publisher;
+    }
+
     public void loadAll() {
         try {
             RabbitManagement.loadAll("http://" + host + ":" + "15672", username, password);
         } catch (Exception e) {
             log.error("Error during loading Rabbit management connections.", e);
         }
+    }
+
+    private void connect() {
+        try {
+            RabbitClient rabbitClient = new RabbitClient();
+            rabbitClient.auth(host, username, password);
+            rabbitClient.connect("ADB_" + UUID.randomUUID().toString());
+            MessagingChannel channel = rabbitClient.openChannel();
+
+            channel.createQueue("acuitybotting.work.discord-bot", false)
+                    .withListener(publisher::publishEvent)
+                    .open(false);
+        } catch (Throwable e) {
+            log.error("Error during dashboard RabbitMQ setup.", e);
+        }
+    }
+
+    @Override
+    public void run(String... strings) throws Exception {
+        connect();
     }
 }
