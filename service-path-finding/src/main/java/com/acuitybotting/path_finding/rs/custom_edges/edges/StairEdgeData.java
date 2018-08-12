522de6a46b228eaea212bd74b5d20334c24227d8
Septron <septron@null.net>
2018-08-12 15:43:41 -0400

    Add more ladders fixes ZachHerridge/Acuity#10

--- service-path-finding/src/main/java/com/acuitybotting/path_finding/rs/custom_edges/edges/StairEdgeData.java
+++ service-path-finding/src/main/java/com/acuitybotting/path_finding/rs/custom_edges/edges/StairEdgeData.java
@@ -11,11 +11,13 @@ public class StairEdgeData {
     private static Collection<CustomEdgeData> connections = new HashSet<>();
 
     static {
+
+        // Wizard's Tower basement ladder
         add((ObstacleEdgeData) new ObstacleEdgeData()
                 .withName("Ladder")
                 .withAction("Climb-down")
                 .withSelection(ObstacleEdgeData.SelectionMode.NEAREST_TO_PLAYER)
-                .setStart(new Location(3104, 3162, 0))
+                .setStart(new Location(3105, 3162, 0))
                 .setEnd(new Location(3104, 9576, 0))
         );
         add((ObstacleEdgeData) new ObstacleEdgeData()
@@ -23,7 +25,39 @@ public class StairEdgeData {
                 .withAction("Climb-up")
                 .withSelection(ObstacleEdgeData.SelectionMode.NEAREST_TO_PLAYER)
                 .setStart(new Location(3104, 9576, 0))
-                .setEnd(new Location(3104, 3162, 0))
+                .setEnd(new Location(3105, 3162, 0))
+        );
+
+        // Dungeon inside Tree Gnome Village
+        add((ObstacleEdgeData) new ObstacleEdgeData()
+                .withName("Ladder")
+                .withAction("Climb-down")
+                .withSelection(ObstacleEdgeData.SelectionMode.NEAREST_TO_PLAYER)
+                .setStart(new Location(2533, 3156, 0))
+                .setEnd(new Location(2533, 9556, 0))
+        );
+        add((ObstacleEdgeData) new ObstacleEdgeData()
+                .withName("Ladder")
+                .withAction("Climb-up")
+                .withSelection(ObstacleEdgeData.SelectionMode.NEAREST_TO_PLAYER)
+                .setStart(new Location(2533, 9556, 0))
+                .setEnd(new Location(2533, 3156, 0))
+        );
+
+        //Falador dwarven mine, east entrance
+        add((ObstacleEdgeData) new ObstacleEdgeData()
+                .withName("Ladder")
+                .withAction("Climb-down")
+                .withSelection(ObstacleEdgeData.SelectionMode.NEAREST_TO_PLAYER)
+                .setStart(new Location(3058, 3376, 0))
+                .setEnd(new Location(3059, 9776, 0))
+        );
+        add((ObstacleEdgeData) new ObstacleEdgeData()
+                .withName("Ladder")
+                .withAction("Climb-up")
+                .withSelection(ObstacleEdgeData.SelectionMode.NEAREST_TO_PLAYER)
+                .setStart(new Location(3059, 9776, 0))
+                .setEnd(new Location(3058, 3376, 0))
         );
     }
 
