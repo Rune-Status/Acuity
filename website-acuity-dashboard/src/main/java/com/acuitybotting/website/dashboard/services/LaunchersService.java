cbf7d37e618827fc01bff05440e9d00779f59301
Zach Herridge <zgherridge@gmail.com>
2018-08-15 12:10:21 -0500

    Launch deploy.

--- website-acuity-dashboard/src/main/java/com/acuitybotting/website/dashboard/services/LaunchersService.java
+++ website-acuity-dashboard/src/main/java/com/acuitybotting/website/dashboard/services/LaunchersService.java
@@ -1,13 +1,24 @@
 package com.acuitybotting.website.dashboard.services;
 
+import com.acuitybotting.common.utils.connection_configuration.ConnectionConfigurationUtil;
+import com.acuitybotting.common.utils.connection_configuration.domain.ConnectionConfiguration;
+import com.acuitybotting.data.flow.messaging.services.client.exceptions.MessagingException;
 import com.acuitybotting.db.arango.acuity.rabbit_db.domain.gson.GsonRabbitDocument;
 import com.acuitybotting.db.arango.acuity.rabbit_db.domain.sub_documents.LauncherConnection;
+import com.acuitybotting.db.arango.acuity.rabbit_db.domain.sub_documents.Proxy;
+import com.acuitybotting.db.arango.acuity.rabbit_db.domain.sub_documents.RsAccountInfo;
 import com.acuitybotting.db.arango.acuity.rabbit_db.service.RabbitDbService;
+import com.acuitybotting.website.dashboard.DashboardRabbitService;
 import com.acuitybotting.website.dashboard.utils.Authentication;
+import com.acuitybotting.website.dashboard.utils.Notifications;
+import com.google.gson.Gson;
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.annotation.SessionScope;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 /**
@@ -18,9 +29,11 @@ import java.util.stream.Collectors;
 public class LaunchersService {
 
     private final RabbitDbService rabbitDbService;
+    private final DashboardRabbitService rabbitService;
 
-    public LaunchersService(RabbitDbService rabbitDbService) {
+    public LaunchersService(RabbitDbService rabbitDbService, DashboardRabbitService rabbitService) {
         this.rabbitDbService = rabbitDbService;
+        this.rabbitService = rabbitService;
     }
 
     public Set<LauncherConnection> loadLaunchers() {
@@ -30,5 +43,32 @@ public class LaunchersService {
                 .filter(connection -> connection.getSubKey().startsWith("ABL_") && (boolean) connection.getHeaders().getOrDefault("connected", false))
                 .map(gsonRabbitDocument -> gsonRabbitDocument.getSubDocumentAs(LauncherConnection.class))
                 .collect(Collectors.toSet());
+    }
+
+    public void deploy(Set<String> subIds, String command, RsAccountInfo rsAccountInfo, Proxy proxy) {
+        Notifications.display("Deploying to {} launchers.", subIds.size());
+
+        ConnectionConfiguration connectionConfiguration = new ConnectionConfiguration();
+        connectionConfiguration.setConnectionId(UUID.randomUUID().toString());
+
+        Map<String, Object> launchConfig = new HashMap<>();
+        launchConfig.put("command", command);
+        launchConfig.put("acuityConnectionConfiguration", ConnectionConfigurationUtil.encode(connectionConfiguration));
+
+        String launchJson = new Gson().toJson(launchConfig);
+        for (String subId : subIds) {
+            String queue = "user." + Authentication.getAcuityPrincipalId() + ".queue." + subId;
+            try {
+                rabbitService.getRabbitChannel().buildMessage(
+                        "",
+                        queue,
+                        launchJson
+                ).setAttribute("type", "runCommand").send();
+            } catch (MessagingException e) {
+                e.printStackTrace();
+            }
+        }
+
+        Notifications.display("Deployment complete.");
     }
 }
--- website-acuity-dashboard/src/main/java/com/acuitybotting/website/dashboard/views/connections/launchers/LaunchClientsComponent.java
+++ website-acuity-dashboard/src/main/java/com/acuitybotting/website/dashboard/views/connections/launchers/LaunchClientsComponent.java
@@ -1,20 +1,14 @@
 package com.acuitybotting.website.dashboard.views.connections.launchers;
 
-import com.acuitybotting.common.utils.connection_configuration.ConnectionConfigurationUtil;
-import com.acuitybotting.common.utils.connection_configuration.domain.ConnectionConfiguration;
-import com.acuitybotting.data.flow.messaging.services.client.exceptions.MessagingException;
 import com.acuitybotting.db.arango.acuity.rabbit_db.domain.sub_documents.LauncherConnection;
 import com.acuitybotting.db.arango.acuity.rabbit_db.domain.sub_documents.Proxy;
 import com.acuitybotting.db.arango.acuity.rabbit_db.domain.sub_documents.RsAccountInfo;
-import com.acuitybotting.website.dashboard.DashboardRabbitService;
 import com.acuitybotting.website.dashboard.components.general.separator.TitleSeparator;
 import com.acuitybotting.website.dashboard.services.AccountsService;
+import com.acuitybotting.website.dashboard.services.LaunchersService;
 import com.acuitybotting.website.dashboard.services.ProxiesService;
-import com.acuitybotting.website.dashboard.utils.Authentication;
 import com.acuitybotting.website.dashboard.utils.Components;
 import com.acuitybotting.website.dashboard.utils.Layouts;
-import com.acuitybotting.website.dashboard.utils.Notifications;
-import com.google.gson.Gson;
 import com.vaadin.flow.component.AttachEvent;
 import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.icon.VaadinIcon;
@@ -24,10 +18,7 @@ import com.vaadin.flow.spring.annotation.SpringComponent;
 import com.vaadin.flow.spring.annotation.UIScope;
 import lombok.Getter;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Set;
-import java.util.UUID;
 import java.util.stream.Collectors;
 
 @SpringComponent
@@ -35,10 +26,10 @@ import java.util.stream.Collectors;
 @Getter
 public class LaunchClientsComponent extends VerticalLayout {
 
-    private final DashboardRabbitService rabbitService;
     private final LauncherListComponent launcherListComponent;
     private final ProxiesService proxiesService;
     private final AccountsService accountsService;
+    private final LaunchersService launchersService;
 
     private ComboBox<Proxy> proxyComboBox = new ComboBox<>();
     private ComboBox<RsAccountInfo> accountComboBox = new ComboBox<>();
@@ -46,11 +37,11 @@ public class LaunchClientsComponent extends VerticalLayout {
     private TextField commandField = new TextField();
     private String defaultCommand = "{RSPEER_JAVA_PATH} -Dacuity.connection={CONNECTION} -Djava.net.preferIPv4Stack=true -jar \"{RSPEER_SYSTEM_HOME}RSPeer/cache/rspeer.jar\"";
 
-    public LaunchClientsComponent(DashboardRabbitService rabbitService, LauncherListComponent launcherListComponent, ProxiesService proxiesService, AccountsService accountsService) {
-        this.rabbitService = rabbitService;
+    public LaunchClientsComponent(LauncherListComponent launcherListComponent, ProxiesService proxiesService, AccountsService accountsService, LaunchersService launchersService) {
         this.launcherListComponent = launcherListComponent;
         this.proxiesService = proxiesService;
         this.accountsService = accountsService;
+        this.launchersService = launchersService;
     }
 
     @Override
@@ -87,33 +78,11 @@ public class LaunchClientsComponent extends VerticalLayout {
 
     private void deploy() {
         Set<String> subIds = launcherListComponent.getSelectedValues().map(LauncherConnection::getSubKey).collect(Collectors.toSet());
-
-        Notifications.display("Deploying to {} launchers.", subIds.size());
-
-        Map<String, Object> launchConfig = new HashMap<>();
-        launchConfig.put("command", commandField.getValue());
-
-
-        ConnectionConfiguration connectionConfiguration = new ConnectionConfiguration();
-        connectionConfiguration.setConnectionId(UUID.randomUUID().toString());
-
-        launchConfig.put("acuityConnectionConfiguration", ConnectionConfigurationUtil.encode(connectionConfiguration));
-
-        String launchJson = new Gson().toJson(launchConfig);
-        for (String subId : subIds) {
-            String queue = "user." + Authentication.getAcuityPrincipalId() + ".queue." + subId;
-            try {
-                rabbitService.getRabbitChannel().buildMessage(
-                        "",
-                        queue,
-                        launchJson
-                ).setAttribute("type", "runCommand").send();
-            } catch (MessagingException e) {
-                e.printStackTrace();
-            }
-
-        }
-
-        Notifications.display("Deployment complete.");
+        launchersService.deploy(
+                subIds,
+                commandField.getValue(),
+                accountComboBox.getOptionalValue().orElse(null),
+                proxyComboBox.getOptionalValue().orElse(null)
+        );
     }
 }
