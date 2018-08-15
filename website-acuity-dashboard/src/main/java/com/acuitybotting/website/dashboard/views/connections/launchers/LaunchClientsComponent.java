3f5c016f62b96ede8cc06c894b76403ec66687dd
Zach Herridge <zgherridge@gmail.com>
2018-08-15 11:19:15 -0500

    Launcher account/proxy selection.

--- website-acuity-dashboard/src/main/java/com/acuitybotting/website/dashboard/views/connections/launchers/LaunchClientsComponent.java
+++ website-acuity-dashboard/src/main/java/com/acuitybotting/website/dashboard/views/connections/launchers/LaunchClientsComponent.java
@@ -2,22 +2,25 @@ package com.acuitybotting.website.dashboard.views.connections.launchers;
 
 import com.acuitybotting.data.flow.messaging.services.client.exceptions.MessagingException;
 import com.acuitybotting.db.arango.acuity.rabbit_db.domain.sub_documents.LauncherConnection;
+import com.acuitybotting.db.arango.acuity.rabbit_db.domain.sub_documents.Proxy;
+import com.acuitybotting.db.arango.acuity.rabbit_db.domain.sub_documents.RsAccountInfo;
 import com.acuitybotting.website.dashboard.DashboardRabbitService;
 import com.acuitybotting.website.dashboard.components.general.separator.TitleSeparator;
+import com.acuitybotting.website.dashboard.services.AccountsService;
+import com.acuitybotting.website.dashboard.services.ProxiesService;
 import com.acuitybotting.website.dashboard.utils.Authentication;
 import com.acuitybotting.website.dashboard.utils.Components;
 import com.acuitybotting.website.dashboard.utils.Layouts;
 import com.acuitybotting.website.dashboard.utils.Notifications;
 import com.google.gson.Gson;
 import com.vaadin.flow.component.AttachEvent;
+import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.spring.annotation.SpringComponent;
 import com.vaadin.flow.spring.annotation.UIScope;
 import lombok.Getter;
-import org.springframework.stereotype.Component;
-import org.springframework.web.context.annotation.SessionScope;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -31,13 +34,20 @@ public class LaunchClientsComponent extends VerticalLayout {
 
     private final DashboardRabbitService rabbitService;
     private final LauncherListComponent launcherListComponent;
+    private final ProxiesService proxiesService;
+    private final AccountsService accountsService;
+
+    private ComboBox<Proxy> proxyComboBox = new ComboBox<>();
+    private ComboBox<RsAccountInfo> accountComboBox = new ComboBox<>();
 
     private TextField commandField = new TextField();
     private String defaultCommand = "{RSPEER_JAVA_PATH} {CENV_VARIABLES} -Djava.net.preferIPv4Stack=true -jar \"{RSPEER_SYSTEM_HOME}RSPeer/cache/rspeer.jar\"";
 
-    public LaunchClientsComponent(DashboardRabbitService rabbitService, LauncherListComponent launcherListComponent) {
+    public LaunchClientsComponent(DashboardRabbitService rabbitService, LauncherListComponent launcherListComponent, ProxiesService proxiesService, AccountsService accountsService) {
         this.rabbitService = rabbitService;
         this.launcherListComponent = launcherListComponent;
+        this.proxiesService = proxiesService;
+        this.accountsService = accountsService;
     }
 
     @Override
@@ -50,15 +60,25 @@ public class LaunchClientsComponent extends VerticalLayout {
 
             add(
                     new TitleSeparator("Command"),
-                    Layouts.wrapHorizontal("100%",
+                    Layouts.wrapHorizontal("65%",
                             commandField,
                             Components.button(VaadinIcon.REFRESH, event -> commandField.setValue(defaultCommand))
                     )
             );
 
-            add(new TitleSeparator("Deploy"), Components.button("Start Client(s)", event -> deploy()));
+            proxyComboBox.setWidth("65%");
+            proxyComboBox.setItemLabelGenerator(proxy -> proxy.getHost() + ":" + proxy.getPort() + (proxy.getUsername() == null ? "" : " " + proxy.getUsername()));
+            add(new TitleSeparator("Proxy"), proxyComboBox);
+
+            accountComboBox.setWidth("65%");
+            accountComboBox.setItemLabelGenerator(RsAccountInfo::getSubKey);
+            add(new TitleSeparator("Account"), accountComboBox);
+
+            add(new TitleSeparator("Deploy"), Components.button("Launch", event -> deploy()));
         }
 
+        proxyComboBox.setItems(proxiesService.loadProxies());
+        accountComboBox.setItems(accountsService.loadAccounts());
         launcherListComponent.load();
     }
 
